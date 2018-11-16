11860a2b9b58531ea87727df14ebf4294434d80c
gittauros <zhy12081992@126.com>
2018-11-16 19:14:38 +0800

    spring boot web

--- src/main/java/com/tauros/kaleido/aron/Application.java
+++ src/main/java/com/tauros/kaleido/aron/Application.java
@@ -3,12 +3,14 @@ package com.tauros.kaleido.aron;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Created by zhy on 2018/10/26.
  */
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
