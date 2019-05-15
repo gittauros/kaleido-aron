2d2afb1fe88cacda566220d756cfcccc3a73aaa3
gittauros <zhy12081992@126.com>
2019-05-15 15:47:30 +0800

    redis

--- src/main/java/com/tauros/kaleido/aron/config/RedisConfig.java
+++ src/main/java/com/tauros/kaleido/aron/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.net.UnknownHostException;
 
@@ -17,12 +18,26 @@ import java.net.UnknownHostException;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(
-            RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        FastJsonRedisSerializer fastJsonRedisSerializer = fastJsonRedisSerializer();
+        StringRedisSerializer stringRedisSerializer = stringRedisSerializer();
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
-        template.setValueSerializer(new FastJsonRedisSerializer<>(Object.class));
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(fastJsonRedisSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    public FastJsonRedisSerializer fastJsonRedisSerializer() {
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        return fastJsonRedisSerializer;
+    }
+
+    @Bean
+    public StringRedisSerializer stringRedisSerializer() {
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        return stringRedisSerializer;
     }
 }
