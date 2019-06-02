fd68da786e0b671e29b84e6c3304fb7716f26b6b
gittauros <zhy12081992@126.com>
2019-06-02 16:12:46 +0800

    nacos

--- src/main/java/com/tauros/kaleido/aron/Application.java
+++ src/main/java/com/tauros/kaleido/aron/Application.java
@@ -1,19 +1,18 @@
 package com.tauros.kaleido.aron;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by zhy on 2018/10/26.
  */
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 @EnableCaching
 public class Application {
 
