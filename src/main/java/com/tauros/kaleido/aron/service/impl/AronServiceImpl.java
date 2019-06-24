45b3acd0c0cb1013424b9fc71b2192805f3d1305
gittauros <zhy12081992@126.com>
2019-06-24 18:33:52 +0800

    dubbo nacos

--- src/main/java/com/tauros/kaleido/aron/service/impl/AronServiceImpl.java
+++ src/main/java/com/tauros/kaleido/aron/service/impl/AronServiceImpl.java
@@ -3,7 +3,7 @@ package com.tauros.kaleido.aron.service.impl;
 import com.tauros.kaleido.aron.repository.RedisRepository;
 import com.tauros.kaleido.aron.service.AronService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 import javax.annotation.Resource;
 import java.io.IOException;
@@ -12,7 +12,7 @@ import java.io.IOException;
  * @author zhy
  * @date 2019/6/21
  */
-@Service
+@Service(version = "1.0.0")
 @Slf4j
 public class AronServiceImpl implements AronService {
 
