996aee0b44cbe8259339e3c828cc9f6984dd44f6
gittauros <zhy12081992@126.com>
2019-06-25 17:49:50 +0800

    dubbo nacos

--- src/main/java/com/tauros/kaleido/aron/Application.java
+++ src/main/java/com/tauros/kaleido/aron/Application.java
@@ -4,9 +4,9 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by zhy on 2018/10/26.
