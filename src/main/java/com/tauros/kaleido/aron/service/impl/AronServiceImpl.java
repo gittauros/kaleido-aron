b52bb9d0d66c798314f1367844bfa12914a17266
gittauros <zhy12081992@126.com>
2019-06-25 15:50:27 +0800

    dubbo nacos

--- src/main/java/com/tauros/kaleido/aron/service/impl/AronServiceImpl.java
+++ src/main/java/com/tauros/kaleido/aron/service/impl/AronServiceImpl.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  * @author zhy
  * @date 2019/6/21
  */
-@Service(version = "1.0.0")
+//@Service(version = "1.0.0")
 @Slf4j
 public class AronServiceImpl implements AronService {
 
