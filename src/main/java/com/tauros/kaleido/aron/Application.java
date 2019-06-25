ceafad81c6c0d8dff9087fde2bd3137bdafbfd60
gittauros <zhy12081992@126.com>
2019-06-25 13:02:29 +0800

    dubbo nacos

--- src/main/java/com/tauros/kaleido/aron/Application.java
+++ src/main/java/com/tauros/kaleido/aron/Application.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableDiscoveryClient
-@EnableDubbo(scanBasePackages = "com.tauros.kaleido.aron.service")
+@EnableDubbo(scanBasePackages = "com.tauros.kaleido.aron")
 @EnableCaching
 public class Application {
 
